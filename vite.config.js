31f0a1d917ba23fb65ffe39be889609be95083e1
dong <964669546@qq.com>
2024-11-23 13:12:09 +0800

    修改配置信息

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "node:path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -18,6 +17,14 @@ export default defineConfig({
         target: "http://localhost:18080", //后台服务所在的地址
         changeOrigin: true, //修改地址
         rewrite: (path) => path.replace(/^\/api/, ""), // 重写请求路径，将 /api 开头的请求去掉 /api
+      },
+    },
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ["legacy-js-api"],
       },
     },
   },
