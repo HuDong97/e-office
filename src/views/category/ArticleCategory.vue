e84a997e62402485cedd6896d9a2716292ea6f6e
dong <964669546@qq.com>
2025-02-20 10:46:00 +0800

    添加鼠标悬浮特效

--- src/views/category/ArticleCategory.vue
+++ src/views/category/ArticleCategory.vue
@@ -128,22 +128,24 @@ const title = ref("");
       <el-table-column label="使用次数" prop="categoryCount"></el-table-column>
       <el-table-column label="操作" width="100">
         <template #default="{ row }">
-          <el-button
-            :icon="Edit"
-            circle
-            plain
-            type="primary"
-            @click="showDialog(row)"
-            title="编辑分类"
-          ></el-button>
-          <el-button
-            :icon="Delete"
-            circle
-            plain
-            type="danger"
-            @click="deleteCategory(row)"
-            title="删除分类"
-          ></el-button>
+          <el-tooltip content="编辑分类" popper-class="custom-tooltip">
+            <el-button
+              :icon="Edit"
+              circle
+              plain
+              type="primary"
+              @click="showDialog(row)"
+            ></el-button>
+          </el-tooltip>
+          <el-tooltip content="删除分类" popper-class="custom-tooltip">
+            <el-button
+              :icon="Delete"
+              circle
+              plain
+              type="danger"
+              @click="deleteCategory(row)"
+            ></el-button>
+          </el-tooltip>
         </template>
       </el-table-column>
 
